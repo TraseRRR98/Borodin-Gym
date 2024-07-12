2081b226defae65de15bdba9374e7a56206d9392
TraseRRR98 <traserrr@gmail.com>
2024-07-12 19:41:41 -0400

    Update navbar link in manage_fighters/info.php

--- pages/manage_fighters/info.php
+++ pages/manage_fighters/info.php
@@ -56,7 +56,7 @@ if (isset($_GET['id'])) {
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container-fluid">
-            <a class="navbar-brand" href="../index.php">Borodin Gym</a>
+            <a class="navbar-brand" href="../../index.php">Borodin Gym</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
